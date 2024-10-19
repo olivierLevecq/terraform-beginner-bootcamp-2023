c1268edea5a3051cca8110c7e54a79c415fc7522
Olivier Levecq <olivier.levecq@gmail.com>
2024-10-19 22:59:42 +0000

    #11 s3 bucket creation

--- main.tf
+++ main.tf
@@ -4,18 +4,28 @@ terraform {
       source = "hashicorp/random"
       version = "3.6.3"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.72.1"
+    }
   }
 }
 
-provider "random" {
+provider "aws" {
   # Configuration options
 }
 
 resource "random_string" "bucket_name" {
-    length           = 16
-    special          = false
+    length  = 32
+    special = false
+    lower   = true
+    upper   = false
   }
 
 output "random_bucket_name" {
   value = random_string.bucket_name.id
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.id
 }
